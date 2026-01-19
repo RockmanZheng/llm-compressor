85f9eaacd8b298a86e92e2eeb46be076638ac83b
zhenglewen <zhenglewen@zhenglewendeMacBook-Pro.local>
2026-01-19 15:00:38 +0800

    [BUGFIX] KVCacheScaleType - change import path - possibly caused by version mismatch

--- src/llmcompressor/modifiers/quantization/cache.py
+++ src/llmcompressor/modifiers/quantization/cache.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List, Optional, Tuple
 
-from compressed_tensors.quantization.lifecycle import KVCacheScaleType
+from compressed_tensors.quantization import KVCacheScaleType
 from compressed_tensors.quantization.quant_args import QuantizationArgs
 from torch import Tensor
 from transformers import DynamicCache
